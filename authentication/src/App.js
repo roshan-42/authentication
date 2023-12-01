17ad4e34a7193b5f95615e9cd6fbd997b174157a
Roshan Acharya <roshanacharya207@gmail.com>
2023-12-01 20:12:02 +0545

    authentication

---
+++ authentication/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>React Authentication</h1> */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
